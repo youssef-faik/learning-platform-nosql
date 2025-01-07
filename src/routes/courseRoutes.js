5a76739b67f5283acb0b02e9173dbc98cb243777
Youssef Faik <faik.yusef@gmail.com>
2025-01-08 00:27:47 +0100

    Refactor course routes

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -8,8 +8,9 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 
 // Routes pour les cours
+router.get('/stats', courseController.getCourseStats);  
+router.get('/', courseController.getAllCourses);
 router.post('/', courseController.createCourse);
 router.get('/:id', courseController.getCourse);
-router.get('/stats', courseController.getCourseStats);
 
 module.exports = router;
