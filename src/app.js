fde6ac8e8a668b0d6ccc999638ea900ac7cd97e8
Youssef Faik <faik.yusef@gmail.com>
2025-01-08 15:12:36 +0100

    Initialize DB connections and configure Express middleware

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 // Question: Comment organiser le point d'entrée de l'application ?
 // Question: Quelle est la meilleure façon de gérer le démarrage de l'application ?
 
+require('dotenv').config();
 const express = require('express');
 const config = require('./config/env');
 const db = require('./config/db');
 
 const courseRoutes = require('./routes/courseRoutes');
-const studentRoutes = require('./routes/studentRoutes');
 
 const app = express();
 
 async function startServer() {
   try {
-    // TODO: Initialiser les connexions aux bases de données
-    // TODO: Configurer les middlewares Express
-    // TODO: Monter les routes
-    // TODO: Démarrer le serveur
+    await db.connectMongo();
+    await db.connectRedis();
+
+    app.use(express.json());
+    app.use('/courses', courseRoutes);
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
@@ -24,7 +29,8 @@ async function startServer() {
 
 // Gestion propre de l'arrêt
 process.on('SIGTERM', async () => {
-  // TODO: Implémenter la fermeture propre des connexions
+  await db.closeConnection();
+  process.exit(0);
 });
 
 startServer();
